82f6be60425809ab2283b4bfa43b34062df2487a
João Dematte <demattejoao@gmail.com>
2024-02-28 13:33:09 -0300

    day 2 aoc

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,20 @@ use aoc::solutions;
 use std::fs;
 
 fn main() {
-    let file_content =
-        fs::read_to_string("./inputs/day01.txt").expect("Should have been able to read the file");
+    // let file_content =
+    //     fs::read_to_string("./inputs/day01.txt").expect("Should have been able to read the file");
 
-    let result_part_one = solutions::day01::resolve_part_one(file_content.clone());
-    let result_part_two = solutions::day01::resolve_part_two(file_content);
+    // let result_part_one = solutions::day01::resolve_part_one(file_content.clone());
+    // let result_part_two = solutions::day01::resolve_part_two(file_content);
+
+    // println!("Result of part one: {}", result_part_one);
+    // println!("Result of part two: {}", result_part_two);
+
+    let file_content =
+        fs::read_to_string("./inputs/day02.txt").expect("Should have been able to read the file");
+
+    let result_part_one = solutions::day02::resolve_part_one(file_content.clone());
+    let result_part_two = solutions::day02::resolve_part_two(file_content);
 
     println!("Result of part one: {}", result_part_one);
     println!("Result of part two: {}", result_part_two);
